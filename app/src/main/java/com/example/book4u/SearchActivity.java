8607629850683ef798a77cb520f9ba20007d547b
JoaoVitor615 <joao.peniza@etec.sp.gov.br>
2022-06-06 22:57:05 -0300

    Implementando mais atributos na activity de busca

--- app/src/main/java/com/example/book4u/SearchActivity.java
+++ app/src/main/java/com/example/book4u/SearchActivity.java
@@ -3,12 +3,16 @@ package com.example.book4u;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,12 +26,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
     private EditText nomeLivro;
     private TextView nomeTitulo;
     private TextView nomeAutor;
-
+    private TextView nomePag;
+    private TextView nomeCat;
+    private Button buttonLink;
+    public String stringLink = null;
 
     ImageButton btnVoltar;
     @Override
@@ -38,6 +46,9 @@ public class SearchActivity extends AppCompatActivity implements LoaderManager.L
         nomeLivro = findViewById(R.id.inputLivro);
         nomeTitulo = findViewById(R.id.tituloText);
         nomeAutor = findViewById(R.id.autorText);
+        nomePag = findViewById(R.id.pagText);
+        nomeCat = findViewById(R.id.catText);
+        buttonLink = findViewById(R.id.btnLink);
         if (getSupportLoaderManager().getLoader(0) != null) {
             getSupportLoaderManager().initLoader(0, null, this);
         }
@@ -48,6 +59,24 @@ public class SearchActivity extends AppCompatActivity implements LoaderManager.L
                 finish();
             }
         });
+
+        buttonLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(stringLink != null){
+                    gotoUrl(stringLink);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Sem Link disponível", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
+    private void gotoUrl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
 
     public void buscaLivros(View view) {
@@ -101,28 +130,62 @@ public class SearchActivity extends AppCompatActivity implements LoaderManager.L
             int i = 0;
             String titulo = null;
             String autor = null;
+            String pag = null;
+            String cat = null;
+            String link = null;
+
             while (i < itemsArray.length() &&
                     (autor == null && titulo == null)) {
                 JSONObject book = itemsArray.getJSONObject(i);
                 JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
                 try {
                     titulo = volumeInfo.getString("title");
                     autor = volumeInfo.getString("authors");
+                    pag = volumeInfo.getString("pageCount");
+                    cat = volumeInfo.getString("categories");
+                    link = volumeInfo.getString("previewLink");
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
                 i++;
             }
             if (titulo != null && autor != null) {
+
                 nomeTitulo.setText(titulo);
+
+                autor = autor.replaceAll("\\[", "");
+                autor = autor.replaceAll("\\]", "");
+                autor = autor.replaceAll("\\\"", "");
                 nomeAutor.setText(autor);
+
+                nomePag.setText("N° de páginas: " + pag);
+
+                if(cat != null){
+                    cat = cat.replaceAll("\\[", "");
+                    cat = cat.replaceAll("\\]", "");
+                    cat = cat.replaceAll("\\\"", "");
+                    nomeCat.setText("Categoria: " + cat);
+                } else {
+                    nomeCat.setText("Categoria: Não Identificado");
+                }
+
+                stringLink = link;
+
             } else {
                 nomeTitulo.setText(R.string.sem_resultado);
                 nomeAutor.setText(R.string.vazio);
+                Toast.makeText(getApplicationContext(),"else", Toast.LENGTH_SHORT).show();
+
             }
         } catch (Exception e) {
-            nomeTitulo.setText(R.string.sem_resultado);
+            nomeTitulo.setText(R.string.vazio);
             nomeAutor.setText(R.string.vazio);
+            nomeCat.setText(R.string.vazio);
+            nomePag.setText(R.string.vazio);
+            stringLink = null;
+            Toast.makeText(getApplicationContext(),"Nenhum livro encontrado", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
